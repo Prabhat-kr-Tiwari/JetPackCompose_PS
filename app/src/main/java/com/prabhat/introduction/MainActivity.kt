a81e5bb0aaf738776bad92770bc22bfacd29cd03
Prabhat kumar tiwari <sonukbc6@gmail.com>
2024-06-11 17:06:51 +0530

    toolbar

--- app/src/main/java/com/prabhat/introduction/MainActivity.kt
+++ app/src/main/java/com/prabhat/introduction/MainActivity.kt
@@ -8,9 +8,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,15 +28,47 @@ import androidx.compose.ui.unit.dp
 import com.prabhat.introduction.ui.theme.IntroductionTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             IntroductionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(), topBar = {
+                        TopAppBar(title = {
+                            Text(text = "Demo App")
+
+                        }, navigationIcon = {
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
+                            }
+                        }, actions = {
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(imageVector = Icons.Filled.Notifications, contentDescription = null)
+                            }
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(imageVector = Icons.Filled.Search, contentDescription = null)
+                            }
+                        }
+                        )
+                    }, floatingActionButton = {
+                        FloatingActionButton(onClick = { /*TODO*/ }) {
+                            
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(imageVector = Icons.Filled.Add , contentDescription = "")
+
+                            }
+                            
+                        }
+                    }
+
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding).size(91.dp)
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .size(91.dp)
                     )
                 }
             }
@@ -42,12 +84,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier,
         color = MaterialTheme.colorScheme.primary,
 
-    )
+        )
 }
 
-@Preview(showSystemUi = true,showBackground = true, name = "light mode")
-@Preview(showBackground = true, name = "Dark mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showSystemUi = true, showBackground = true, name = "light mode")
+@Preview(
+    showBackground = true, name = "Dark mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
 @Composable
 fun GreetingPreview() {
     IntroductionTheme {
